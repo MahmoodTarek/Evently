e327c3ace8150d1c1398df768a22eae144bf4c8f
DELL <dev.mahmoudtarek@gmail.com>
2026-03-26 12:00:06 +0200

    Add SVG icons and placeholder images for profile and logout
    Added:
    - `ic_logout.svg`
    - `ic_right_arrow.svg` and `ic_right_arrow_dark.svg`
    - `profile_place_holder.png` and `profile_place_holder_dark.png` with 2.0x and 3.0x resolutions.

--- lib/utils/resources/app_assets.dart
+++ lib/utils/resources/app_assets.dart
@@ -1,11 +1,25 @@
-class AppIcons {
+abstract class AppIcons {
   static const selectedHome = 'assets/icons/selected_home_tab.svg';
   static const selectedHomeDark = 'assets/icons/selected_home_tab_dark.svg';
   static const unselectedHome = 'assets/icons/unselected_home_tab.svg';
   static const selectedFavorite = 'assets/icons/selected_favorite_tab.svg';
-  static const selectedFavoriteDark = 'assets/icons/selected_favorite_tab_dark.svg';
+  static const selectedFavoriteDark =
+      'assets/icons/selected_favorite_tab_dark.svg';
   static const unselectedFavorite = 'assets/icons/unselected_favorite_tab.svg';
   static const selectedProfile = 'assets/icons/selected_profile_tab.svg';
-  static const selectedProfileDark = 'assets/icons/selected_profile_tab_dark.svg';
+  static const selectedProfileDark =
+      'assets/icons/selected_profile_tab_dark.svg';
   static const unselectedProfile = 'assets/icons/unselected_profile_tab.svg';
+  static const icRightArrow = 'assets/icons/ic_right_arrow.svg';
+  static const icRightArrowDark = 'assets/icons/ic_right_arrow_dark.svg';
+  static const icLogout = 'assets/icons/ic_logout.svg';
+  static const themeMode = 'assets/icons/lottie_theme_mode.json';
+  static const icSelectedLanguage = 'assets/icons/ic_selected_language.svg';
+
+}
+
+abstract class AppImages {
+  static const placeHolderProfilePic = 'assets/images/profile_place_holder.png';
+  static const placeHolderProfilePicDark =
+      'assets/images/profile_place_holder_dark.png';
 }
