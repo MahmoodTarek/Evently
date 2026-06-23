5dc0d14cf0047bd45a053bd86780f2a0478aca9f
DELL <dev.mahmoudtarek@gmail.com>
2026-06-23 20:01:50 +0300

    Implement user management and sub-collections in Firebase integration.
    - Update `FirebaseUtils` to support a `users` collection and restructure the `events` collection as a sub-collection of a specific user.
    - Enhance `FirebaseAuthService` to create and store user profiles in Firestore upon registration and fetch them during login.
    - Integrate `UserProvider` within authentication flows to maintain the current user's state.
    - Update `EventsProvider` and `FirebaseUtils` methods to require a `uId`, ensuring operations are scoped to the authenticated user.
    - Update dependencies in `pubspec.lock`, including `cloud_firestore`, `firebase_auth`, and various utility packages.

--- lib/data/firebase/auth/firebase_auth_service.dart
+++ lib/data/firebase/auth/firebase_auth_service.dart
@@ -1,9 +1,13 @@
+import 'package:evently/data/firebase_utils/firebase_utils.dart';
+import 'package:evently/model/user.dart';
+import 'package:evently/provider/user_provider.dart';
 import 'package:evently/ui/widgets/custom_dialog.dart';
 import 'package:evently/ui/widgets/toast_message.dart';
 import 'package:evently/utils/app_theme_extension.dart';
 import 'package:evently/utils/resources/app_styles.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FirebaseAuthService {
   static void showLoading({
@@ -20,24 +24,55 @@ class FirebaseAuthService {
   }
 
   static Future<bool> createNewAccountEmailAndPw({
+    required String name,
     required String email,
     required String password,
     required BuildContext context,
   }) async {
     try {
-      showLoading(context: context, message: 'Loading');
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      showLoading(
+        context: context,
+        message: 'Loading',
+      );
+
+      final UserCredential credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
 
+      final String userId = credential.user?.uid ?? '';
+
+      final User user = User(
+        id: userId,
+        name: name,
+        email: email,
+      );
+
+      await FirebaseUtils.addUserToFirebase(user: user);
+
+      final userProvider = Provider.of<UserProvider>(
+        context,
+        listen: false,
+      );
+
+      userProvider.setCurrentUser(user: user);
+
       return true;
     } on FirebaseAuthException {
-      CustomDialog.hideLoading(context);
-      ToastMessage.show(context: context, message: 'Failed to create account');
+      ToastMessage.show(
+        context: context,
+        message: 'Failed to create account',
+      );
       return false;
     } catch (_) {
+      ToastMessage.show(
+        context: context,
+        message: 'Something went wrong',
+      );
       return false;
+    } finally {
+      CustomDialog.hideLoading(context);
     }
   }
 
@@ -47,20 +82,53 @@ class FirebaseAuthService {
     required BuildContext context,
   }) async {
     try {
-      showLoading(context: context, message: 'Loading');
+      showLoading(
+        context: context,
+        message: 'Loading',
+      );
 
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      CustomDialog.hideLoading(context);
+
+      final String userId = userCredential.user!.uid;
+
+      final User? user = await FirebaseUtils.getUserFromFirebase(
+        userId,
+      );
+
+      if (user == null) {
+        ToastMessage.show(
+          context: context,
+          message: 'User not found',
+        );
+        return false;
+      }
+
+      final userProvider = Provider.of<UserProvider>(
+        context,
+        listen: false,
+      );
+
+      userProvider.setCurrentUser(user: user);
+
       return true;
     } on FirebaseAuthException {
-      CustomDialog.hideLoading(context);
-      ToastMessage.show(context: context, message: 'Failed to login');
+      ToastMessage.show(
+        context: context,
+        message: 'Failed to login',
+      );
       return false;
     } catch (_) {
+      ToastMessage.show(
+        context: context,
+        message: 'Something went wrong',
+      );
       return false;
+    } finally {
+      CustomDialog.hideLoading(context);
     }
   }
 }
--- lib/data/firebase_utils/firebase_utils.dart
+++ lib/data/firebase_utils/firebase_utils.dart
@@ -1,9 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:evently/model/event.dart';
+import 'package:evently/model/user.dart';
 
 class FirebaseUtils {
-  static CollectionReference<Event> getEventsCollection() {
+  FirebaseUtils._();
+
+  static CollectionReference<User> getUsersCollection() {
     return FirebaseFirestore.instance
+        .collection(User.collectionName)
+        .withConverter<User>(
+      fromFirestore: (snapshot, _) => User.fromFirestore(snapshot.data()!),
+      toFirestore: (user, _) => user.toFirestore(),
+    );
+  }
+
+  static Future<void> addUserToFirebase({required User user}) {
+    return getUsersCollection().doc(user.id).set(user);
+  }
+
+  static Future<User?> getUserFromFirebase(String userId) async {
+    return await getUsersCollection().doc(userId).get().then((snapshot) {
+      if (snapshot.exists) {
+        return snapshot.data();
+      } else {
+        return null;
+      }
+    });
+  }
+
+  static CollectionReference<Event> getEventsCollection({required String uId}) {
+    return getUsersCollection().doc(uId)
         .collection(Event.collectionName)
         .withConverter<Event>(
           fromFirestore: (snapshot, _) => Event.fromFirestore(snapshot.data()!),
@@ -11,8 +37,9 @@ class FirebaseUtils {
         );
   }
 
-  static Future<void> addEvent({required Event event}) {
-    final collectionRef = getEventsCollection(); // create a new Collection
+  static Future<void> addEvent({required Event event, required String uId}) {
+    final collectionRef = getEventsCollection(
+        uId: uId); // create a new Collection
     var docRef = collectionRef
         .doc(); // Create a new document with an auto-generated ID
     event.id = docRef.id; // Set the ID of the event
